232eca28040740cec863b99cec55db9482ef3db1
Patoche <pastouretpatrick@gmail.com>
2024-03-14 16:09:54 +0100

    OPENSTREETMAP MARCHE

--- src/app/app.Main.ts
+++ src/app/app.Main.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'appMain',
@@ -8,7 +9,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.Main.html',
   styleUrl: './app.Main.css'
 })
-export class AppComponent {
-  title = 'museek';
-}
 
+export class AppComponent implements OnInit {
+  title = 'museek';
+  ngOnInit() {
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+  }
+}
