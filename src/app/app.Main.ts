87dba67bd72c97e5c814d0c190c0d2387b32747c
Yusuf Ulas <yusufulas481@gmail.com>
2024-03-14 16:33:00 +0100

    ZoomFix

--- src/app/app.Main.ts
+++ src/app/app.Main.ts
@@ -16,7 +16,8 @@ export class AppComponent implements OnInit {
     const map = L.map('map').setView([51.505, -0.09], 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
+      minZoom: 5,
+      maxZoom: 18,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
   }
