0e7576580423be93446e27c57af3e0588a680d08
Dark-Gouloum <49821955+Dark-Gouloum@users.noreply.github.com>
2024-03-18 17:11:46 +0100

    feat(BackEnd): Correction de l'arbre des dossier

---
+++ Back/app/controllers/Events.controller.js
@@ -0,0 +1,39 @@
+const db = require("../index.js");
+const Event = db.events;
+
+/*
+// Create and Save a new Event
+exports.create = (req, res) => {
+
+};
+
+// Retrieve all Events from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single Event with an id
+exports.findOne = (req, res) => {
+  
+};
+
+// Update a Event by the id in the request
+exports.update = (req, res) => {
+  
+};
+
+// Delete a Event with the specified id in the request
+exports.delete = (req, res) => {
+
+};
+
+// Delete all Events from the database.
+exports.deleteAll = (req, res) => {
+
+};
+
+// Find all published Events
+exports.findAllPublished = (req, res) => {
+
+};
+*/
