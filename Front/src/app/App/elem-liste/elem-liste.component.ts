64b95eddbdfaa339d0cf5bf0dddb69a7743dc54b
ABTonniere <91685633+ABTonniere@users.noreply.github.com>
2024-03-25 00:36:23 +0100

    modif elem

--- Front/src/app/App/elem-liste/elem-liste.component.ts
+++ Front/src/app/App/elem-liste/elem-liste.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {ListeComponent} from "../liste/liste.component";
 
 @Component({
   selector: 'app-elem-liste',
@@ -6,10 +7,13 @@ import {Component, Input} from '@angular/core';
   styleUrl: './elem-liste.component.css'
 })
 export class ElemListeComponent {
-  @Input() titre: string = "Hello World !";
-  @Input() description: string = "Lorem ipsum dolor sit amet, consectekefhbsdjkffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffflghds<lfhskjfkysrjfyeufysfiyseiufhsdjifghdsfidfisGHIEGFquipsfhqsufgqsjifgqsfuygqfyuqstgfsuyqgfqsyufgqsuygftur adipiscing elit. Nulla ac purus nec nunc tincidunt aliquam"
-  @Input() lieu: string = "Quelque part dans le monde";
-  @Input() date: string = "Aujourd'hui ou demain ou après-demain ou jamais";
+  @Input() titre: string = "Nom de l'événement";
+  @Input() description: string = "Description de l'événement"
+  @Input() lieu: string = "Lieu de l'événement";
+  @Input() date: string = "Date de l'événement";
+  @Input() type: string = "type de l'événement";
+  @Input() genre: string = "genre de l'événement";
+  @Input() artiste: string = "artiste de l'événement";
 
 
   affichagePlus: boolean = false;
